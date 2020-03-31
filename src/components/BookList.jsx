362b06fd6c4d82061c3f4c7b3f54e4f3e685afe2
Erebos66 <schroeer.gerrit@live.de>
2020-03-31 15:06:25 +0200

    Outsourced Single Book Item Into A Separate Component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,22 +3,25 @@ import styled from 'styled-components';
 import { MdAddCircle } from 'react-icons/md';
 import { BookContext } from '../contexts/BookContext';
 import { AppContext } from '../contexts/AppContext';
+import BookSingle from './BookSingle';
 
 function BookList() {
-  const { books, dispatch } = useContext(BookContext);
+  const { books } = useContext(BookContext);
   const { toggleModal } = useContext(AppContext);
 
   return (
     <Wrapper>
       <span className='add-icon' onClick={toggleModal}><MdAddCircle /></span>
-      { books.length === 0 && <h3 className='reading-status'>Your list is empty, take a break</h3>}
-      <div className='list'>
-        <ul>
-          {
-            books.map((book) => <li onClick={() => dispatch({ type: 'removeBook', id: book.id })} key={book.id}><span>{book.title}<small> (by {book.author})</small></span><span>{book.category}</span></li>)
+      { books.length ? (
+        <div className='list'>
+          <ul>
+            {
+            books.map((book) => <BookSingle book={book} />)
           }
-        </ul>
-      </div>
+          </ul>
+        </div>
+      )
+        : <h3 className='reading-status'>Your list is empty, take a break</h3>}
     </Wrapper>
   );
 }
@@ -61,26 +64,6 @@ const Wrapper = styled.div`
     ul {
       list-style-type: none;
       padding: 0;
-
-      li {
-        display: flex;
-        background-color: inherit;
-        justify-content: space-between;
-        align-items: center;
-        font-size: 1.3rem;
-        padding: 20px 0;
-        transition: .1s;
-        cursor: pointer;
-        &:not(:last-of-type){
-          border-bottom: 1px solid #151515;
-        }
-
-        &:hover {
-          transform: scale(1.1);
-          border-bottom: none;
-          text-decoration: line-through;
-        }
-      }
     }
   }
   `;
